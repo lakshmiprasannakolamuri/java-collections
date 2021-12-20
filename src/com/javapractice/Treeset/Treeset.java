fcec6aac0795f75f02ed0fc40f5d4618a9de6db1
LP <lakshmiprasannakolamuri3@gmail.com>
2021-12-20 19:37:19 +0530

    Treesets

--- src/com/javapractice/Treeset/Treeset.java
+++ src/com/javapractice/Treeset/Treeset.java
@@ -1,5 +1,6 @@
 package com.javapractice.Treeset;
 
+import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class Treeset
@@ -10,11 +11,33 @@ public class Treeset
     TreeSet<Integer> treeset = new TreeSet<>();
     treeset.add(5);
     treeset.add(6);
-    treeset.add(7);
+    treeset.add(10);
     treeset.add(8);
     treeset.add(9);
     
     System.out.println(treeset);
+    
+    TreeSet<String> treeset1 = new TreeSet<>();
+    treeset1.add("A");
+    treeset1.add("B");
+    treeset1.add("P");
+    treeset1.add("D");
+    treeset1.add("V");
+    
+    System.out.println(treeset1);
+    System.out.println(treeset1.subSet("P", "V"));
+    
+    
+    TreeMap<String, Double> treemap = new TreeMap<>();
+    treemap.put("Apple", 30.0);
+    treemap.put("Orange", 45.2);
+    treemap.put("Papaya", 154.3);
+    treemap.put("kiwi", 22.9);
+    treemap.put("Banana", 55.7);
+    
+    System.out.println(treemap);
+    
+    
     
 	}
 
